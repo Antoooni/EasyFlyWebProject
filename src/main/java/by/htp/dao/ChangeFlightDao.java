7383fbdfe58c0e2ff4be5468e9e28b0c8430cbe1
Ayumazec <mazets_Anton@tut.by>
2017-07-07 21:13:08 +0300

    test git in idea

--- src/main/java/by/htp/dao/ChangeFlightDao.java
+++ src/main/java/by/htp/dao/ChangeFlightDao.java
@@ -4,7 +4,7 @@ import by.htp.bin.Flight;
 import by.htp.exception.DAOException;
 
 public interface ChangeFlightDao {
-	//public Flight changeFlightDepartureDate();
+	//public Flight changeFlightDepartureDate() ;
 	public void cancelFlight(Flight flight) throws DAOException;
 	public void changeFlightData(Flight flight) throws DAOException;
 }
