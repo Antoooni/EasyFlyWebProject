5944bb7a3a2f80fcafbad9deb118f8712c6e3996
Antoooni <mazets_anton@tut.by>
2017-07-10 17:36:28 +0300

    Added Dao, which get users which flights were changed

--- src/main/java/by/htp/dao/EmailDao.java
+++ src/main/java/by/htp/dao/EmailDao.java
@@ -6,9 +6,6 @@ import by.htp.bin.User;
 
 import java.util.List;
 
-/**
- * Created by Ayumazec on 07.07.2017.
- */
 public interface EmailDao {
     public List<User> selectUserEmail(Flight flight) throws DAOException;
 }
